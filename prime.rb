462dbf3fa358dc410dcb332f3c0de5b6906a91aa
Lisa Hufnagel <lmhufnagel@gmail.com>
2017-08-05 21:07:58 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
-# Add  code here!
+
+def sieve(max)
+  prime_num = (0..max).to_a
+
+  prime_num[0] = prime_num[1] = nil
+
+  counter = 0
+  prime_num.each do |integer|
+    next unless integer
+    break if integer*integer > max
+    counter += 1
+
+    (integer*integer).step(max,integer) { |multiple| prime_num[multiple] = nil }
+  end
+
+prime_num
+end
+
+def prime?(num)
+  sieve(num).include?(num)
+end
+
+puts prime?(15)
